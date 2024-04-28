9876a21fed44b13403bfdc3558241b52a7d4799a
009-KumarJi <krishnakumar091@yahoo.com>
2024-04-28 14:32:49 +0530

    socket.io connected to client and server'

--- server/server.js
+++ server/server.js
@@ -2,15 +2,28 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from 'cors'
 
 const app = express();
 dotenv.config({path: './.env'});
 
 const server = createServer(app);
-const io = new Server(server, {});
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5000",
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 export const mode = process.env.NODE_ENV.trim() || 'PRODUCTION';
+
+app.use(cors({
+    origin: "http://localhost:5000",
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -21,6 +34,6 @@ io.on("connection", (socket) => {
 
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} in ${mode} mode.`);
 });
